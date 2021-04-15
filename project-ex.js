042eef3f33b197069f70f05a08355ab538d82930
Tran Quang Dinh <tranquangdinhtcdt@gmail.com>
2021-04-15 14:46:29 +0700

    thuc hanh tinh dien tich

---
+++ project-ex.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt('The width is: ');
+inputHeight = prompt ('The height is: ');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write('The area is: ' + area);
